33c24febc2abaffa2686537aa74cc636f13aef09
Kyle Miller <kylesmiller38@gmail.com>
2019-03-18 18:26:37 -0600

    added javadoc comments

--- src/Items/Wallet.java
+++ src/Items/Wallet.java
@@ -1,21 +1,49 @@
 package Items;
 
+/**
+ * Store and manage the money of any Character, Faction, or Location
+ * @author Kyle Miller
+ */
 public class Wallet {
 
-    private long balance = 0;
-    private int[] formattedBalance = new int[5];
+    private int[] formattedBalance = new int[5];  // follows [pp, gp, ep, sp, cp] format
+    private long balance = 0;  // value of the wallet in cp
 
-    public Wallet(boolean autoGenerate){
-        if (autoGenerate){
-            generateBalance();
-        }
+    /**
+     * Constructor for Wallets automatically created for Characters
+     * @param ownerWealth the wealth modifier of the parent Character or Location
+     */
+    public Wallet(float ownerWealth){
+        generateBalance(ownerWealth);
+    }
+
+    /**
+     * Constructor for manually creating a Wallet
+     */
+    public Wallet(int pp, int gp, int ep, int sp, int cp){
+        setCoinage(pp, gp, ep, sp, cp);
     }
 
 
+    /**
+     * Getter methods
+     */
+    public long getBalance(){
+        return this.balance;
+    }
+
+    public int[] getFormattedBalance(){
+        return this.formattedBalance;
+    }
+
+
+
+    /**
+     * Alter the balance of the wallet
+     * @param newBalance an array in the [pp, gp, ep, sp, cp] format
+     * @param setTo indicates whether to override the existing wallet with the new array, or add the values to it
+     */
     public void changeBalance(int[] newBalance, boolean setTo){
-        // if wallet is not auto-generated, this method is used to create an initial balance
-        // change the integer balance of the wallet
-        // newBalance follows [pp, gp, ep, sp, cp] format
         long intAmount = coinageToRaw(newBalance);
         if (setTo) {
             this.balance = intAmount;
@@ -27,10 +55,11 @@ public class Wallet {
     }
 
 
+    /**
+     * Set the exact coins of the wallet
+     * This is method is only called when a DM manually sets the coins in a wallet
+     */
     public void setCoinage(int pp, int gp, int ep, int sp, int cp){
-        // Set the exact coins of the wallet
-        // This is method is only used when a DM manually sets the coins in a wallet
-
         this.formattedBalance[0] = pp;
         this.formattedBalance[1] = gp;
         this.formattedBalance[2] = ep;
@@ -41,15 +70,21 @@ public class Wallet {
     }
 
 
-    private void generateBalance(){
+    /**
+     * Automatically set the values in the wallet to an amount appropriate for the owner
+     * @param wealthModifier modifier used to generate the starting balance of the wallet
+     */
+    private void generateBalance(float wealthModifier){
         // TODO: wealth auto-generation logic -- need owner wealth logic before this can be implemented
     }
 
 
+    /**
+     * Convert balance integer into formatted coinage array
+     * EP and PP are ignored when calculating the coinage
+     * Only used in auto-generation of wallets
+     */
     private void calcCoinage() {
-        // Convert balance integer into formatted coinage array
-        // EP and PP are ignored when calculating the coinage
-        // This method is only used in auto-generation of wallets
         long tempBalance = this.balance;
         int[] autoFormattedBalance = {this.formattedBalance[1], this.formattedBalance[3], this.formattedBalance[4]};
 
@@ -73,8 +108,11 @@ public class Wallet {
     }
 
 
+    /**
+     * Convert the formatted array of coins into an integer
+     * @param coinage an array in the [pp, gp, ep, sp, cp] format
+     */
     private int coinageToRaw(int[] coinage){
-        // Convert the formatted array of coins into an integer
         int rawValue = 0;
         rawValue += coinage[0] * 1000;        // add PP
         rawValue += coinage[1] * 100;         // add GP
@@ -82,16 +120,6 @@ public class Wallet {
         rawValue += coinage[3] * 10;          // add SP
         rawValue += coinage[4];               // add CP
         return rawValue;
-    }
-
-
-    public long getBalance(){
-        return this.balance;
-    }
-
-
-    public int[] getFormattedBalance(){
-        return this.formattedBalance;
     }
 }
 
