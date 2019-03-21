6c34003c4e33d691b389383a154091c1d3d8e2a6
Kyle Miller <kylesmiller38@gmail.com>
2019-03-20 20:51:41 -0600

    Expanded on Item class framework, converted Item to an abstract class

--- src/Items/Item.java
+++ src/Items/Item.java
@@ -2,7 +2,7 @@ package Items;
 
 
 
-public class Item {
+public abstract class Item {
 
     private String name = "Unnamed Item";
     private boolean magical = false;
@@ -10,45 +10,18 @@ public class Item {
     private int[] statBlockMod = {0, 0, 0, 0, 0, 0};
     private String traits = "No traits";
     private String description = "No description";
+    private int basePrice = 0;
+    private int weight = 0;
+    private String summary = "No description";
 
     public Item(String name){
         this.name = name;
     }
 
 
-    /*
-    ------------- Setter methods -------------
-    */
-    public void setName(String newName){
-        this.name = newName;
-        // TODO: add max name length -- need UI draft to determine max length
-    }
-
-    public void toggleMagical(){
-        this.magical = !this.magical;
-    }
-
-    public void setRarity(int rareValue){
-        this.rarity = rareValue;
-    }
-
-    public void setStatBlock(int[] statMods){
-        for (int i = 0; i < statMods.length; i ++){
-            statBlockMod[i] = statMods[i];
-        }
-    }
-
-    public void setTraits(String text){
-        this.traits = text;
-    }
-
-    public void setDescription(String text){
-        this.description = text;
-    }
-
-    /*
-    ------------- Getter methods -------------
-    */
+    /**
+     * getter methods
+     */
     public String getName(){
         return this.name;
     }
@@ -80,4 +53,67 @@ public class Item {
     public String getDescription() {
         return description;
     }
+
+    public int getBasePrice(){
+        return basePrice;
+    }
+
+    public int getWeight(){
+        return weight;
+    }
+
+    public String getSummary(){
+        return summary;
+    }
+
+    /**
+     * setter methods
+     */
+    public void setName(String newName){
+        this.name = newName;
+        // TODO: add max name length -- need UI draft to determine max length
+    }
+
+    public void toggleMagical(){
+        this.magical = !this.magical;
+    }
+
+    public void setRarity(int rareValue){
+        //TODO: Get max and min rarities from an actual area
+        int minRarity = 0;
+        int maxRarity = 5;
+        if (rareValue < minRarity){
+            this.rarity = 0;
+        }
+        else if(rareValue > maxRarity){
+            this.rarity = 5;
+        }
+        else{
+            this.rarity = rareValue;
+        }
+    }
+
+    public void setStatBlock(int[] statMods){
+        for (int i = 0; i < statMods.length; i ++){
+            statBlockMod[i] = statMods[i];
+        }
+    }
+
+    public void setTraits(String text){
+        this.traits = text;
+    }
+
+    public void setDescription(String text){
+        this.description = text;
+    }
+
+    public void setBasePrice(int price){
+        this.basePrice = price;
+    }
+
+    public void setWeight(int weight){
+        this.weight = weight;
+    }
+
+    protected abstract void writeSummary();
 }
