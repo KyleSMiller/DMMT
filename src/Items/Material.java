30aab654cdca535bf2763535f1635efdb12f3700
Kyle Miller <kylesmiller38@gmail.com>
2019-03-23 16:56:07 -0600

    Set up getter and setter methods

--- src/Items/Material.java
+++ src/Items/Material.java
@@ -1,4 +1,48 @@
 package Items;
 
-public class Material {
+import java.util.ArrayList;
+
+public class Material extends Item{
+
+    private ArrayList<Item> partOf;
+
+    public Material(String name){
+        super();
+        this.setName(name);
+        this.partOf = new ArrayList<Item>();
+    }
+
+    public Material(){
+        super();
+        this.setName("Unnamed Material");
+        this.partOf = new ArrayList<Item>();
+    }
+
+
+    public ArrayList<Item> getPartOf(){
+        return this.partOf;
+    }
+
+    /**
+     * Add any Item to the list of Items this material composes
+     * @param part the item to add to the list
+     */
+    public void addPartOf(Item part){
+        this.partOf.add(part);
+    }
+
+    /**
+     * Remove any Item from the list of Items this material composes
+     * @param part the item to remove from the list
+     */
+    public void removePartOf(Item part){
+        this.partOf.remove(part);
+    }
+
+
+    @Override
+    public String writeSummary(){
+        return "Placeholder Summary";
+    }
+
 }
