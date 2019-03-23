549d11bcffcc249ff6e8435e7a1aed5e851b329a
Kyle Miller <kylesmiller38@gmail.com>
2019-03-23 16:33:01 -0600

    Set up getter and setter methods

--- src/Items/RangedWeapon.java
+++ src/Items/RangedWeapon.java
@@ -1,4 +1,42 @@
 package Items;
 
-public class RangedWeapon {
+
+public class RangedWeapon extends Weapon {
+
+    private boolean loading;
+    // private Ammunition ammunition;
+
+    public RangedWeapon(String name){
+        super();
+        this.setName(name);
+        this.loading = false;
+        // this.ammunition = new Ammunition("Arrows")
+    }
+
+    /**
+     * Getter methods
+     */
+    public boolean isLoading(){
+        return this.loading;
+    }
+
+//    public boolean getAmmunition(){
+//        return this.Ammunition;
+//    }
+
+    /**
+     * Setter Methods
+     */
+    public void toggleLoading(){
+        this.loading = !this.loading;
+    }
+//
+//    public void setAmmunition(){
+//        //TODO: Proper ammo changing logic
+//    }
+
+    @Override
+    public String writeSummary(){
+        return "Placeholder Summary";
+    }
 }
